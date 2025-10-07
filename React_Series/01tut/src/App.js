7790f703668d5f89c5140a29e308c71ed59aedbc
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-07 19:59:32 +0530

    app js

--- React_Series/01tut/src/App.js
+++ React_Series/01tut/src/App.js
@@ -1,4 +1,5 @@
 import Content from './Content';
+import Footer from './Footer';
 import Header from './Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     
        <Header />
        <Content />
+       <Footer />
     </div>
   );
 }
