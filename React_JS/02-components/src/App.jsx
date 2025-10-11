a03d0de943c93b719c1b9ab165a4bb8e10367f49
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-11 11:08:24 +0530

    app jsx

--- React_JS/02-components/src/App.jsx
+++ React_JS/02-components/src/App.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <div className="card">
+          <h1>MD FAZAL</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quibusdam.</p>
+      </div>
+      <div className="card">
+          <h1>MD FAZAL</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quibusdam.</p>
+      </div>
+      <div className="card">
+          <h1>MD FAZAL</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quibusdam.</p>
+      </div>
+      <div className="card">
+          <h1>MD FAZAL</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quibusdam.</p>
+      </div>
     </div>
   )
 }
