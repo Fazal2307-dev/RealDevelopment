52e74bc33d1b7840aef6b7cd7e4c16aef7374851
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-11 22:03:29 +0530

    dynamically chnage image through props

--- React_JS/03_props/src/App.jsx
+++ React_JS/03_props/src/App.jsx
@@ -4,9 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='fazal' age={25}/>
-      <Card user='afzal' age={18}/>
-      <Card user='aman' age={25}/>
+      <Card user='fazal' age={25} img="https://images.unsplash.com/photo-1758846182585-c37d8c2946c2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"/>
+      <Card user='afzal' age={18}img="https://images.unsplash.com/photo-1668765592438-8a37d2cef565?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764"/>
+      <Card user='aman' age={25}img="https://images.unsplash.com/photo-1608979174671-54608f734528?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687"/>
 
      
     </div>
