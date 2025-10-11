399700fe16c9c2f584d39ebd2e3391436f2c3bb6
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-11 20:38:38 +0530

    03_props

---
+++ React_JS/03_props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Hello md fazal</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
