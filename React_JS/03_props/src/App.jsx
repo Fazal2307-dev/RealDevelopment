90f56d127e8b5400ba360828cb66fd9561ee05e5
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-11 21:56:05 +0530

    dynamic calling through props

--- React_JS/03_props/src/App.jsx
+++ React_JS/03_props/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className='parent'>
-      <div className="card">
-        <img src="https://images.unsplash.com/photo-1756990683708-afd38ed20f1f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764"
-         alt="" />
-        <h1>Hello md fazal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
+      <Card user='fazal' age={25}/>
+      <Card user='afzal' age={18}/>
+      <Card user='aman' age={25}/>
+
+     
     </div>
   )
 }
