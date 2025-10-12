ae032cac7b276f3961584c76612a80d8d16a79ab
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-12 10:16:33 +0530

    learning props through card components

--- React_JS/04-cards-project/src/App.jsx
+++ React_JS/04-cards-project/src/App.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h2>fazal</h2>
+    <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="https://imgs.search.brave.com/nKz_jFlZWYDvJs7oFiBzJECedcrSHw9cOGwsTgYKZXg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvNTAwcC8w/MS8xMC9hbWF6b24t/bG9nby1ibGFjay1h/bmQteWVsbG93LXZl/Y3Rvci00Njg2MDEx/MC5qcGc" alt="" />
+          <button>save</button>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="bottom">
+          
+        </div>
+      </div>
+      
     </div>
   )
 }
