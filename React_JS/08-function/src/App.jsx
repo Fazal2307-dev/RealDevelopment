9698012f86991347d1c86cc9dfff6fa60912069e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-14 21:42:19 +0530

    getting value as function

--- React_JS/08-function/src/App.jsx
+++ React_JS/08-function/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const App = () => {
+  const inputChanging = (val)=>{
+    console.log(val)
+  }
   
   return (
     <div>
-      <input  onChange={()=>{
-        console.log('user is typing........')
+      <input  onChange={function(elem){
+        inputChanging(elem.target.value)
       }}    type="text" placeholder='Enter Name' className='bg-gray-400 p-4 m-4 text-m' />
     </div>
   )
