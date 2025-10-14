deb1eefe85f1b22ff52e95b450fcc3ce75aa1bbf
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-14 21:22:47 +0530

    changing text using function

--- React_JS/08-function/src/App.jsx
+++ React_JS/08-function/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const App = () => {
+  const btnClicked = ()=>{
+    console.log("Hey Fazal you clicked the btn")
+  }
   return (
     <div>
-      hey
+      <button onClick={btnClicked} className='bg-gray-400 p-2 m-3 rounded-full'>Click me</button>
     </div>
   )
 }
