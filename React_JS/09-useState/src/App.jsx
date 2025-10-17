94b4cb6cb1941e4df0b27381183f90e969a00842
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-17 09:44:34 +0530

    increment and decrement

--- React_JS/09-useState/src/App.jsx
+++ React_JS/09-useState/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
- 
-  const [a,setA] = useState(0);
-  const chnage = ()=>{
-    setA()
-  }
+ const[val,setVal] = useState(0)
+
+ const increaseNum = ()=>{
+// console.log("increaseNum");
+ setVal(val+1)
+
+ }
+ const decreaseNum = ()=>{
+  // console.log("decreaseNum");
+  setVal(val-1)
+ }
+ const jumpby5=()=>{
+  setVal(val+5)
+ }
+
   return (
-    <div >
-      <h1>change value is {a}</h1>
-      <button onClick={chnage} className='bg-amber-500'>value</button>
+    <div>
+      <h1>value is={val}</h1>
+      <button  onClick={increaseNum}  className='p-5 m-6 bg-gray-400 rounded-2xl'>increment</button>
+      <button  onClick={decreaseNum} className='p-5 m-6 bg-gray-400 rounded-2xl'>Decrement</button>
+      <button  onClick={jumpby5} className='p-5 m-6 bg-gray-400 rounded-2xl'>jumpby5</button>
     </div>
   )
 }
