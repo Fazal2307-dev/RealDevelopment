59e0a1c328e6aaef35e25605c63c537872843544
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-17 09:25:44 +0530

    learning useState

--- React_JS/09-useState/src/App.jsx
+++ React_JS/09-useState/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+ 
+  const [a,setA] = useState(0);
+  const chnage = ()=>{
+    setA()
+  }
   return (
-    <div className='bg-red-400 m-3 p-1'>
-      hello from react
+    <div >
+      <h1>change value is {a}</h1>
+      <button onClick={chnage} className='bg-amber-500'>value</button>
     </div>
   )
 }
