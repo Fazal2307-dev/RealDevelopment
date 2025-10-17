a42eacd5607fa1b9811dfea6832372c5b70b3fce
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-17 10:32:03 +0530

    changing value through useState

--- React_JS/10-useState-Advance/src/App.jsx
+++ React_JS/10-useState-Advance/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const[num,setNum]=useState({
+    Name:"Fazal",
+    age:25,
+  })
+  const btnClicked= ()=>{
+     const newNum = {...num}
+     newNum.Name = "Afzal";
+     setNum(newNum);
+  }
   return (
     <div>
-      <h1>Hello Fazal</h1>
+      <h1>value is {num.Name}</h1>
+      <button onClick={btnClicked}>Click me </button>
     </div>
   )
 }
