0c078857d37fa9f3d203738a104002762fb8dc7b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-18 10:16:48 +0530

    from handling in react

--- React_JS/11-form-handling/src/App.jsx
+++ React_JS/11-form-handling/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const App = () => {
+const formHandler = (e) =>{
+    e.preventDefault()
+      console.log("Form Submitted");      
+  }
   return (
     <div>
-      
+      <form  onSubmit={(e)=>{
+          formHandler(e)
+        }}>
+        <input type="text" placeholder='Enter Your Name' />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
