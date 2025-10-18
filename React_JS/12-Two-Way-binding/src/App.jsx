a399b9688276d9ee42ccef08a781042eaeb8397c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-18 11:00:42 +0530

    notes-app

--- React_JS/12-Two-Way-binding/src/App.jsx
+++ React_JS/12-Two-Way-binding/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const[title,setTitle]= useState('')
+  const formHandle = (e)=>{
+    e.preventDefault()
+    console.log("form submitted by",title);
+    setTitle('')
+    
+  }
   return (
     <div>
-      
+      <form  onSubmit={(e)=>{formHandle(e)}}>
+        <input 
+        type="text" 
+        placeholder='Enter Your Name'
+        value={title}
+        onChange={(e)=>{
+         setTitle( e.target.value)
+        }} 
+        />
+        
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
