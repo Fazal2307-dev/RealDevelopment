bdadd4abb02728d1e05292c31c91b1569f68b3ff
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-19 18:21:22 +0530

    building notes app

--- React_JS/13-Notes-app/src/App.jsx
+++ React_JS/13-Notes-app/src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      Notes Making App
+      <form className='flex items-center'>
+        <input type="text" placeholder='Enter your Task' />
+        <textarea placeholder='Enter details' name="" id=""></textarea>
+      </form>
     </div>
   )
 }
