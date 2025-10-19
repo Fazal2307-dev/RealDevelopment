6c5e13fb01ef78f7e8b1eb1b2ff6ccabcad3a79b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-19 18:56:02 +0530

    app jsx

--- React_JS/13-Notes-app/src/App.jsx
+++ React_JS/13-Notes-app/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const submitHandle = (e)=>{
+    e.preventDefault();
+    console.log("Form submitted");
+    
+  }
   return (
     <div className='h-screen bg-black text-white'>
-      <form className='flex items-start flex-col p-10 gap-4'>
+      <form onSubmit={(e)=>{submitHandle(e)}}
+      className='flex items-start flex-col p-10 gap-4'>
         <input className='px-5 w-1/2 py-2 font-bold  rounded border-2' type="text" 
         placeholder='Enter your Task' />
         <textarea className='px-5 w-1/2 font-medium py-2 h-32 rounded border-2 ' type="text"
