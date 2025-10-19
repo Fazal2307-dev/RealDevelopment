6ea069fe4a782740cd10f5065a8817918ec70c93
Fazal2307-dev <mdfazal2307@gmail.com>
2025-10-19 18:48:56 +0530

    building notes app

--- React_JS/13-Notes-app/src/App.jsx
+++ React_JS/13-Notes-app/src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <form className='flex items-center'>
-        <input type="text" placeholder='Enter your Task' />
-        <textarea placeholder='Enter details' name="" id=""></textarea>
+    <div className='h-screen bg-black text-white'>
+      <form className='flex items-start flex-col p-10 gap-4'>
+        <input className='px-5 w-1/2 py-2 font-bold  rounded border-2' type="text" 
+        placeholder='Enter your Task' />
+        <textarea className='px-5 w-1/2 font-medium py-2 h-32 rounded border-2 ' type="text"
+         placeholder='Enter Details' />
+         <button className='bg-white w-1/2 h-18 font-bold text-black px-5 py-2 rounded border-2'>Add Notes</button>
       </form>
     </div>
   )
