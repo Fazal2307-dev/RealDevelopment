c1247d4d2d0081b7e039a7c2a64e84c2a7a700d6
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-05 20:36:04 +0530

    sending object in localStorage

--- React_JS/17-LocaalStorage/src/App.jsx
+++ React_JS/17-LocaalStorage/src/App.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 const App = () => {
   //locaalstorage Clear
   // localStorage.clear();
-  localStorage.setItem('user','fazal')
-  localStorage.setItem('age','18')
-  const users= localStorage.getItem('user')
-  const age = localStorage.getItem('age');
+  // localStorage.setItem('user','fazal')
+  // localStorage.setItem('age','18')
+  // const users= localStorage.getItem('user')
+  // const age = localStorage.getItem('age');
 
-  console.log(users,age);
+  const user = {
+    name:'Fazal',
+    age:24,
+    city:"Barh"
+  }
+  localStorage.setItem("user",JSON.stringify(user));
+  const users = JSON.parse(localStorage.getItem(user));
+  console.log(users);
+  
   
   return (
     <div>
