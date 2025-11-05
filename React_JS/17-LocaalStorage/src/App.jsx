1add2ab3ef70634fc09e8e073978e5b7ae6a3bca
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-05 20:25:51 +0530

    localStorage

--- React_JS/17-LocaalStorage/src/App.jsx
+++ React_JS/17-LocaalStorage/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const App = () => {
+  //locaalstorage Clear
+  // localStorage.clear();
+  localStorage.setItem('user','fazal')
+  const users= localStorage.getItem('user')
+  console.log(users);
+  
   return (
     <div>
       LocalStorage
