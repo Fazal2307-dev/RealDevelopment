4c8cbeb1ed1463adac2100dcacad894aa9767251
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-05 20:27:11 +0530

    localStorage for getitem

--- React_JS/17-LocaalStorage/src/App.jsx
+++ React_JS/17-LocaalStorage/src/App.jsx
@@ -4,8 +4,11 @@ const App = () => {
   //locaalstorage Clear
   // localStorage.clear();
   localStorage.setItem('user','fazal')
+  localStorage.setItem('age','18')
   const users= localStorage.getItem('user')
-  console.log(users);
+  const age = localStorage.getItem('age');
+
+  console.log(users,age);
   
   return (
     <div>
