de929ece9f22dda480c56aea95789d338a5c420a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-06 21:38:22 +0530

    using axios fro learning api

--- React_JS/18_api_calling/src/App.jsx
+++ React_JS/18_api_calling/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Axios from 'axios'
 
 const App = () => {
 
     const  getData =  async ()=> {
-    const api = await fetch('https://jsonplaceholder.typicode.com/users')
-    const response = await api.json()
-    console.log(response);
+      //using fetch method for api
+    // const api = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const response = await api.json()
+    // console.log(response);
+    // using axios method for api
+    const response = await Axios('https://jsonplaceholder.typicode.com/users')
+    console.log(response.data);
     
   }
   return (
