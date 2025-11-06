68f4e15980bc5a08fb9afb03237c02d8f7f1a61e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-06 21:22:29 +0530

    learning about api

--- React_JS/18_api_calling/src/App.jsx
+++ React_JS/18_api_calling/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+
+    async function getData (){
+    const api = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    console.log(api);
+    
+  }
   return (
     <div>
-      
+     <button onClick={getData}>Get Data</button>
     </div>
   )
 }
