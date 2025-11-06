dbbf7db43021376d44cc21f56b4c9f445e789f45
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-06 22:57:40 +0530

    learning useEffect

--- React_JS/18_api_calling/src/App.jsx
+++ React_JS/18_api_calling/src/App.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import Axios from 'axios'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
 
+  const [data,setData] = useState([])
     const  getData =  async ()=> {
       //using fetch method for api
     // const api = await fetch('https://jsonplaceholder.typicode.com/users')
     // const response = await api.json()
     // console.log(response);
     // using axios method for api
-    const response = await Axios('https://jsonplaceholder.typicode.com/users')
-    console.log(response.data);
+    // const response = await Axios.get('https://jsonplaceholder.typicode.com/users')
+    // console.log(response.data);
     
+    const response = await axios.get("https://picsum.photos/v2/list")
+   setData(response.data)
+    
+
   }
   return (
     <div>
      <button onClick={getData}>Get Data</button>
+     <div>
+      {data.map(function(elem,idx){
+          return  <h1>hello,{elem.author},{idx}</h1>
+      })}
+      
+     </div>
     </div>
   )
 }
