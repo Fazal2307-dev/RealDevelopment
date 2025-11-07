d15f5abd5e5c386334d6c4c11f9265266cc7fb8f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-07 12:16:02 +0530

    learning about useEffect

--- React_JS/19_useEffect/src/App.jsx
+++ React_JS/19_useEffect/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect } from 'react'
+import { useState } from 'react'
+
 
 const App = () => {
+
+const [a, setA] = useState(0)
+const [b, setB] = useState(0)
+
+  function aChanging(){
+    console.log('A value is chnage ');
+  }
+  function bChanging(){
+    console.log('B value is chnage ');
+  }
+  useEffect(function(){
+    aChanging(),
+    bChanging()
+  },[a])
   return (
-    <div className='text'>
-      Learning about useEffect
+    <div>
+      <h3>value of {a}{}</h3>
+      <h3>value of {b}</h3>
+      <button onClick={()=>{setA(a+1)}}>A</button>
+      <button onClick={()=>{setB(b+1)}}>B</button>
     </div>
   )
 }
