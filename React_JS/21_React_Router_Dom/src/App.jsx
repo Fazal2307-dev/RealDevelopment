b5018e5c2d6855805bdcafbb95a8adf8c4c83470
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-07 22:28:00 +0530

    practices in routing

--- React_JS/21_React_Router_Dom/src/App.jsx
+++ React_JS/21_React_Router_Dom/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import Products from './pages/Products'
 
 const App = () => {
   return (
     <div>
-      Hello from React-Router-dom
+        <Navbar />
+        <Routes>
+          <Route path ='/' element={<Home />}/>
+          <Route path ='/contact' element={<Contact />}/>
+          <Route path ='/about' element={<About />}/>
+          <Route path='/products' element={<Products />} />
+        </Routes>
     </div>
   )
 }
