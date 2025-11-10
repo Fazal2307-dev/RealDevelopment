58f9c032cbed4b29ba075a5fc0489f09427b4379
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-10 19:21:05 +0530

    22-routing advance

--- React_JS/22-Routing-Advance/src/App.jsx
+++ React_JS/22-Routing-Advance/src/App.jsx
@@ -1,23 +1,9 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Courses from './pages/Courses'
-import Home from './pages/Home'
-import Read from './pages/Read'
-import DSASheet from './pages/DSASheet'
-import Playground from './pages/Playground'
-import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-     <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/read' element={<Read />} />
-        <Route path='/dsasheet' element={<DSASheet/>} />
-        <Route path='/playground' element={<Playground />} />
-      </Routes>
+      
     </div>
   )
 }
