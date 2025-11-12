83615954b2f61576a4c3677216e732e8ec249360
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-12 18:25:20 +0530

    Learning Advance routing

--- React_JS/22-Routing-Advance/src/App.jsx
+++ React_JS/22-Routing-Advance/src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Homes from './pages/Homes'
-import Content from './pages/Content'
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Homes />}/>
-        <Route path='/content' element={<Content />} />
-      </Routes>
+      
     </div>
   )
 }
